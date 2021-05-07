a2ac22bf2ea7604f8abde89bec1407d7aea1daf0
joemyson-santos <joemyson1402@gmail.com>
2021-05-07 20:50:47 -0300

    uso do File.readAllLine

--- FileAplication/Program.cs
+++ FileAplication/Program.cs
@@ -8,21 +8,25 @@ namespace FileAplication
         static void Main(string[] args)
 
         {
-            string sourcePath = @"C:caminho do diretorio";
-            string targetPath = @"C:caminho do diretorio";
+            string sourcePath = @"C:\Users\joemy\source\repos\textoTeste.txt";
+            string targetPath = @"C:\Users\joemy\source\repos\textoTeste2.txt";
 
 
             try
             {
                 FileInfo fileinfo = new FileInfo(sourcePath);
                 fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
 
             }
-            catch (IOException e)
+            catch( IOException e)
             {
                 Console.WriteLine("um erro ocorreu");
                 Console.WriteLine(e.Message);
             }
-        }
-    }
+    }}
 }
